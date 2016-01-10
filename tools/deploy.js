85158e13603847b3a3f502a5f5da2765657b26ea
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-01-11 02:11:31 +0700

    Updated deploy URL

--- tools/deploy.js
+++ tools/deploy.js
@@ -7,10 +7,9 @@
 import GitRepo from 'git-repository';
 import task from './lib/task';
 
-// TODO: Update deployment URL
 const remote = {
   name: 'github',
-  url: 'https://github.com/{user}/{repo}.git',
+  url: 'git@github.com:whs/portfolio.git',
   branch: 'gh-pages',
 };
 
